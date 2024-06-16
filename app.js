195822773d782a58fd41ffc3fb090596b75ddf2c
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-06-16 16:10:57 +0700

    feat: lupa require cors (emot tengkorak)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const db = require('./config/db.config')
 
